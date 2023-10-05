dc64b4759787ed4d88d81d31c32074d536cafef7
Apostolos Varelas <87095756+ApostolosVarelas@users.noreply.github.com>
2023-10-06 00:58:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,46 +59,53 @@ function fetchJsonData() {
 }
 
 function generateCards(data) {
-   const cardContainer = document.getElementById('filterable-cards');
-
-   data.forEach(item => {
-       const card = document.createElement('div');
-       card.classList.add('card', 'p-0');
-       card.setAttribute('data-name', item.dataName);
-
-       const img = document.createElement('img');
-       img.src = item.imgSrc;
-       img.alt = 'img';
-
-       const cardBody = document.createElement('div');
-       cardBody.classList.add('card-body');
-
-       const cardTitle = document.createElement('h6');
-       cardTitle.classList.add('card-title');
-       cardTitle.textContent = item.cardTitle;
-
-       cardBody.appendChild(cardTitle);
-       card.appendChild(img);
-       card.appendChild(cardBody);
-       cardContainer.appendChild(card);
-
-       img.addEventListener('click', function() {
-        document.getElementById('modal-img').src = item.imgSrc;
-        document.getElementById('modal-title').textContent = item.cardTitle;
-
-        // Display Price Range with icons and labels
-        const priceTop = item.priceTop || 'N/A';
-        const priceLow = item.priceLow || 'N/A';
-        document.getElementById('modal-priceRange').innerHTML = `<i class="fas fa-tags"></i><span class="label">Price Range:</span> <span class="value">${priceLow} - ${priceTop}</span>`;
-
-        // Display Availability with icons and labels
-        document.getElementById('modal-available').innerHTML = `<i class="fas fa-check-circle"></i><span class="label">Available:</span> <span class="value">${item.available || 'N/A'}</span>`;
-
-        
-        new bootstrap.Modal(document.getElementById('infoModal')).show();
-        });
-   });
-}
+    const cardContainer = document.getElementById('filterable-cards');
+ 
+    const showModal = function() {
+         document.getElementById('modal-img').src = this.imgSrc;
+         document.getElementById('modal-title').textContent = this.cardTitle;
+ 
+         // Display Price Range with icons and labels
+         const priceTop = this.priceTop || 'N/A';
+         const priceLow = this.priceLow || 'N/A';
+         document.getElementById('modal-priceRange').innerHTML = `<i class="fas fa-tags"></i><span class="label">Price Range:</span> <span class="value">${priceLow} - ${priceTop}</span>`;
+ 
+         // Display Availability with icons and labels
+         document.getElementById('modal-available').innerHTML = `<i class="fas fa-check-circle"></i><span class="label">Available:</span> <span class="value">${this.available || 'N/A'}</span>`;
+ 
+         new bootstrap.Modal(document.getElementById('infoModal')).show();
+     };
+ 
+    data.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('card', 'p-0');
+        card.setAttribute('data-name', item.dataName);
+ 
+        const img = document.createElement('img');
+        img.src = item.imgSrc;
+        img.alt = 'img';
+ 
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+ 
+        const cardTitle = document.createElement('h6');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = item.cardTitle;
+ 
+        cardBody.appendChild(cardTitle);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+        cardContainer.appendChild(card);
+ 
+        // Bind the item data to the showModal function
+        const boundShowModal = showModal.bind(item);
+ 
+        // Add event listeners for both click and touchstart
+        img.addEventListener('click', boundShowModal);
+        img.addEventListener('touchstart', boundShowModal);
+    });
+ }
+ 
 
 function sortCards() {
    const cardContainer = document.getElementById('filterable-cards');
